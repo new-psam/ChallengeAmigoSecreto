b93b4ae0ab6deab196233706165ac69f543bbe8c
new-psam <marcelino.patricio@hotmail.com>
2025-01-29 09:39:38 -0500

    array amigos e função para Adiciionar amigos

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá 
 // desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigos(){
+    let amigo = document.querySelector('input').value;
+    if (amigo == ''){
+        alert('Por favor, insira um nome');
+    }else{
+        amigos.push(amigo);
+        console.log(amigos);
+        limparCampo();
+    }
+    
+}
+
+function limparCampo(){
+    amigo = document.querySelector('input');
+    amigo.value = '';
+}
