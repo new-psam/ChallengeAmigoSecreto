09dcf30027ba6bb1dd70cae5e25ee0e56a14c2fc
new-psam <marcelino.patricio@hotmail.com>
2025-01-31 18:17:14 -0500

    implementada a função de sortear o amigo secreto

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 // desenvolver a lógica para resolver o problema.
 
 let amigos = [];
+const lista = document.getElementById('listaAmigos');
+const sorteio = document.getElementById('resultado');
 
 
 function adicionarAmigos(){
@@ -12,11 +14,9 @@ function adicionarAmigos(){
     }else{
         amigos.push(amigo);
         console.log(amigos);
-        limparCampo();
-        
+        limparCampo(); 
         listaDeAmigos();
-    }
-    
+    }   
 }
 
 function limparCampo(){
@@ -25,16 +25,23 @@ function limparCampo(){
 }
 
 
-
 function listaDeAmigos(){
-    let lista = document.getElementById('listaAmigos');
+    //let lista = document.getElementById('listaAmigos');
     
     lista.innerHTML = " ";
     for (let i = 0; i < amigos.length; i++){
 
         lista.innerHTML += `<li>${amigos[i]}</li>`;
     }
-    
-
 }
 
+function sortearAmigo(){
+    if (amigos.length == 0){
+        alert('É preciso inserir amigos para sortear!');
+    }else{
+        let numero = Math.floor(Math.random() * amigos.length);
+        lista.innerHTML = " ";
+        console.log(numero);
+        sorteio.innerHTML = 'O amigo secreto sorteado é:  ' + amigos[numero];
+    }
+} 
